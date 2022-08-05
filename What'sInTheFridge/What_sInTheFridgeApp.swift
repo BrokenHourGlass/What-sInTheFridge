54ecf6c5fa87b31cbe1cf313662aa779258c06a3
Jacob Digby <ShadeSlayerFox@gmail.com>
2022-08-05 09:38:23 -0700

    Initial Commit

---
+++ What'sInTheFridge/What_sInTheFridgeApp.swift
@@ -0,0 +1,17 @@
+//
+//  What_sInTheFridgeApp.swift
+//  What'sInTheFridge
+//
+//  Created by Jacob on 8/5/22.
+//
+
+import SwiftUI
+
+@main
+struct What_sInTheFridgeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
